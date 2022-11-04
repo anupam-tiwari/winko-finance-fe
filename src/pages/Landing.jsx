c1feca2990038e99457a4111cace5e2a0d2b89fa
anupam-tiwari <appleanupam@gmail.com>
2022-11-04 17:36:14 -0400

    46

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import landing1 from '../assets/landing1.png'
+import landing2 from '../assets/landing2.png'
+import landing3 from '../assets/landing3.png'
+import landing4 from '../assets/landing4.png'
 
 
 
@@ -8,7 +12,17 @@ const Landing = () => {
     <div>
         <Navbar></Navbar>
         <hr></hr>
-        <h1 className='text-black p-10 text-3xl'>landing page</h1>
+        <div className='flex justify-center items-center'>
+          <div>
+            <div className='py-8'><img src={landing1}></img></div>
+            <div className='py-8'><img src={landing2}></img></div>
+            <div className='py-8'><img src={landing3}></img></div>
+            <div className='py-8'><img src={landing4}></img></div>
+
+          </div>
+        
+          
+        </div>
     </div>
   )
 }
